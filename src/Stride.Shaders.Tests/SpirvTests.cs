f5c847532be6f4eb77a6ec1d723058d98953c28d
Youness KAFIA <kafia.youness@hotmail.fr>
2026-01-10 15:16:45 +0100

    Added a simple test on link instruction

--- src/Stride.Shaders.Tests/SpirvTests.cs
+++ src/Stride.Shaders.Tests/SpirvTests.cs
@@ -7,6 +7,35 @@ namespace Stride.Shaders.Parsing.Tests;
 public class SpirvTests
 {
 
+
+    [Fact]
+    public void DecorationLinkInstructions()
+    {
+        var buffer = new NewSpirvBuffer();
+
+        var decoration = buffer.Add(new OpDecorateString(1, Decoration.LinkSDSL, "Hello world"));
+        int count = -1;
+        foreach (var operand in decoration)
+        {
+            count++;
+            switch(count, operand.Name)
+            {
+                case (0, "target"):
+                    Assert.Equal(1, operand.ToLiteral<int>());
+                    break;
+                case (1, "decoration"):
+                    Assert.Equal(Decoration.LinkSDSL, operand.ToEnum<Decoration>());
+                    break;
+                case (2, "value"):
+                    Assert.Equal("Hello world", operand.ToLiteral<string>());
+                    break;
+                default:
+                    Assert.Fail($"not the correct operand : {operand.Name} at {count}");
+                    break;
+            };
+        }
+    }
+
     [Fact]
     public void DecorationInstructions()
     {
@@ -19,6 +48,24 @@ public class SpirvTests
             if (operand.Name == "name")
             {
                 Assert.Equal("Hello world", operand.ToLiteral<string>());
+            }
+        }
+    }
+
+    [Fact]
+    public void ConstantValue()
+    {
+        var buffer = new NewSpirvBuffer();
+        buffer.FluentAdd(new OpTypeFloat(1, 32, null), out var floatType)
+        .FluentAdd(new OpConstant<float>(1, 2, 3.0f), out var constant);
+
+        var i = buffer[1];
+        Assert.Equal(4, i.Data.Memory.Length);
+        foreach (var operand in i.Data)
+        {
+            if (operand.Name == "value")
+            {
+                Assert.Equal(3.0f, operand.ToLiteral<float>());
             }
         }
     }
